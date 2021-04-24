436570d8e6b8fd0dff9b2b125fdf9bb7116deb78
Suresh Stalin <ssueresh.saikishore@gmail.com>
2021-04-24 14:55:03 +0530

    added default endpoint

--- src/main/java/com/itgarden/product/TestController.java
+++ src/main/java/com/itgarden/product/TestController.java
@@ -1,13 +1,23 @@
 package com.itgarden.product;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/public/test")
 public class TestController {
 
     @GetMapping
     public String defaultEndPoint() {
         return "Product Service is up and running";
+    }
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping(){
+
+        return new ResponseEntity<>("Product Service is up and running...", HttpStatus.OK);
     }
 }
